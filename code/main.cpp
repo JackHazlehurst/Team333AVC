895c6b86dd7ce98f4a9a19304ccf78c658f92beb
Jackapotamus <jack.hazlehurst>
2017-05-08 03:29:04 +0000

    Camera calculates the averave index of white pixels

--- code/main.cpp
+++ code/main.cpp
@@ -1,15 +1,37 @@
 #include <stdio.h>
 #include <time.h>
-//#include "E101.h" //ENGR101 library
+#include "E101.h" //ENGR101 library
 
-double camera(){
+int camera(int row, int numCols){
 	take_picture();
-	int imageArray [240][320];
-	for(int i = 0; i < 320; i++){
-		
+	display_picture(1,0);
+	char pixelLine[numCols];
+	for(int i = 0; i < numCols; i++){
+		//gets the pixel and makes it white or black
+		int pixel = get_pixel(row, i, 3);
+		if(pixel < 127){
+			pixel = 0;
+		}
+		else{
+			pixel = 1;
+		}
+		pixelLine[i] = pixel;
+		//printf("%d ", pixelLine[i]);
 	}
+    //printf("\n");
+    //calculates the average white pixel index relative to the center collumn 
+    int total;
+    int numWhite;
+    for(int j = 0; j < numCols; j++){
+		if(pixelLine[j] == 1){
+			total = total + pixelLine[j] * (j - (numCols/2));
+			numWhite++;
+		}
+	}
+	return total/numWhite;
 }
 
 int main(){
 	init();//initialises the hardware
+	printf("Average white pixel position %d\n", camera(160, 320));
 }
