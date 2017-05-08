c6f02ac8a54e279295b589305de375feecdb4c3e
Jackapotamus <jack.hazlehurst@gmail.com>
2017-05-08 14:18:34 +1200

    moving to pi

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <time.h>
-#include "E101.h" //ENGR101 library
+//#include "E101.h" //ENGR101 library
+
+double camera(){
+	take_picture();
+	int imageArray [240][320];
+	for(int i = 0; i < 320; i++){
+		
+	}
+}
 
 int main(){
-	inti();//initialises the hardware
+	init();//initialises the hardware
 }
