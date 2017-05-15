f0bdf7ca6f3e1e9e3f1fbb9f2ba12b05ec715883
Dylan Hoefsloot <hoefsldyla@steinburg.ecs.vuw.ac.nz>
2017-05-15 15:08:10 +1200

    sage aborts the commit.

--- code/main.cpp
+++ code/main.cpp
@@ -2,30 +2,32 @@
 #include <time.h>
 #include "E101.h" //ENGR101 library
 
-//will get a row of pixels from the camera 
-int camera(int row, int numCols){
+//will get a row of pixels from the camera
+//accepts the row in the image it needs to look at
+int pixelLine(int row){
+	int THRESHOLD = 127;
+	int WIDTH = 320;
 	take_picture();
 	//display_picture(0,10);
-	char pixelLine[numCols];
-	for(int i = 0; i < numCols; i++){
+	char pixelLine[WIDTH];
+	for(int i = 0; i < WIDTH; i++){
 		//gets the pixel and makes it white or black
 		int pixel = get_pixel(row, i, 3);
-		if(pixel < 127){
+		if(pixel < THRESHOLD){//black
 			pixel = 0;
 		}
-		else{
+		else{//white
 			pixel = 1;
 		}
 		pixelLine[i] = pixel;
-		//printf("%d ", pixelLine[i]);
 	}
     //printf("\n");
     //calculates the average white pixel index relative to the center collumn 
     int total;
     int numWhite;
-    for(int j = 0; j < numCols; j++){
+    for(int j = 0; j < WIDTH; j++){
 		if(pixelLine[j] == 1){
-			total = total + pixelLine[j] * (j - (numCols/2));
+			total = total + pixelLine[j] * (j - (WIDTH/2));
 			numWhite++;
 		}
 	}
@@ -36,23 +38,22 @@ int camera(int row, int numCols){
 	return 0;
 }
 
-//makes the robot move 
-int move(){
-	int speed = 100;
-	set_motor(1, speed);
-	set_motor(2, speed);
-	sleep1(1, 0);
-	set_motor(1, 0);
-	set_motor(2, 0);
-	
-	return 0;
+//makes the robot move
+//accepts the ideal speed, error, and scaling factor as a arguments
+void move(int speed, int error, double factor){
+	//calculates speed for each wheel
+	int leftSpeed = speed + (int)((double)error*factor);
+	int rightSpeed = speed - (int)((double)error*factor);
+	//sets speed for each wheel
+	set_motor(1, leftSpeed);
+	set_motor(2, rightSpeed);
+	sleep1(0, 12500);//80 Hz
 }
 
 int main(){
 	init();//initialises the hardware
+	//Infinite loop for running the robot
 	while(true){
-	int pixelError = camera(160, 320);
-		printf("%d\n", pixelError);
-		sleep1(0,200);
+		move(50, pixelLine(160), 0.25);
 	}
 }
